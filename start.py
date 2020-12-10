354ee0f5d60e9a465219b3e6c99236c354812196
cvatricks <68840422+cvatricks@users.noreply.github.com>
2020-12-10 20:42:11 +0530

    Update start.py

--- start.py
+++ start.py
@@ -32,7 +32,7 @@ async def handler2(event):
         for links in value:
                 link = links
                 get_links.append(link)
-        await client.send_file(chat,random.choice(get_links).replace("\/", "/"),caption = "{}\n\nSee more at {}".format(get_title, see_more))
+        await client.send_file(chat,random.choice(get_links).replace("\/", "/"),caption = "{}\n\nSee more at {}".format(get_title, see_more.replace("\/", "/")))
         return
     #with open("backup.json", "w", encoding="utf8") as outfile:
     #          json.dump(b_json, outfile, ensure_ascii=False)
@@ -53,44 +53,6 @@ async def handler2(event):
    # await client.send_message(chat, g1)
    #await client.send_message(chat,"445")
     
-#@client.on(events.NewMessage(pattern='(?i)https://www.mxplayer.in'))
-async def handler(event):
-    link =event.text.split('/')[-1]
-    video_d = "https://llvod.mxplay.com/"
-    A =requests.get("https://api.mxplay.com/v1/web/detail/video?type=movie&id="+link+"&platform=com.mxplay.desktop&device-density=2&userid=30bb09af-733a-413b-b8b7-b10348ec2b3d&platform=com.mxplay.mobile&content-languages=hi,en,ta").json()
-    chat = await event.get_chat()
-    markup = client.build_reply_markup(Button.url("stream",video_d+A["stream"]['hls']['high']))
-    await client.send_message(chat," support @urlicupload   "+A["title"],buttons=markup)
-    print(A)
-    print(link)
-#@client.on(events.NewMessage(pattern='(?i)https://www.hotstar.com/in/'))
-async def handler(event):
-    link =event.text
-    print(link)
-    #import youtube_dl
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
-    with ydl:
-        result = ydl.extract_info(
-        link,
-        download=True # We just want to extract the info
-    )
-    await client.send_message(chat,result)
-    
-@client.on(events.NewMessage(pattern='(?i)/ls'))
-async def handler(event):
-    link =event.text.split(" ")[1]
-    e = os.listdir(link)
-    chat = await event.get_chat()
-    c = "|"
-    #str1.join(s)
-    #print(c)
-    await client.send_message(chat,c.join(e))
-@client.on(events.NewMessage(pattern='(?i)sm'))
-async def handler(event):
-    link =event.text.split(" ")[1]
-    print(link)
-    chat = await event.get_chat()
-    await client.send_file(chat, '/Download'+link,force_document=True)
     
     
     
