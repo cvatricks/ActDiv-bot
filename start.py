c4952f52c5ffe220c925a16ad8e4119131dca4ff
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-04-15 16:50:02 +0530

    Update start.py

--- start.py
+++ start.py
@@ -12,7 +12,7 @@ async def handler(event):
     chat = await event.get_chat()
     await client.send_message(chat,""" 💁 Join \n\n@tamil_girls_boys_chatting_group""")
 
-@client.on(events.NewMessage(chats=[-523451499]))
+@client.on(events.NewMessage(chats=[-523451499], pattern='/send'))
 async def sendvid(event):
     chat = await event.get_chat()
     await client.send_message(chat,""" OK """)
