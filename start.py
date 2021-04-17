2e8167ec3c2179a47cd668fa42ba5a344794947f
cvatricks <68840422+cvatricks@users.noreply.github.com>
2021-04-17 19:40:31 +0530

    Update start.py

--- start.py
+++ start.py
@@ -16,7 +16,7 @@ async def handler(event):
 async def sendvid(event):
     chat = await event.get_chat()
     content = await event.get_reply_message()
-    await client.send_message(chat, content.id)
+    await client.send_message(chat, content.message)
     await client.send_message(chat, 'Yes or no?', buttons=[
         Button.inline('Yes!', b'yes'),
         Button.inline('Nope', b'no')
